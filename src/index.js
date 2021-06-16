7c0cbc2bcae3ef44350d1476ea67a3ad96853383
Nick <nicholas.schenk@rutgers.edu>
2021-06-16 12:54:57 -0400

    started tic tac toe

--- src/index.js
+++ src/index.js
@@ -31,18 +31,49 @@ import './index.css';
     For example, now we can refer to the whole shopping list by writing <ShoppingList />
 */
 class Square extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={value:null};
+    }
+
+
+    //what does it do tho? it renders a single button!
     render() {
+    /* since we added the prop value when calling renderSquare, we can now access that value in the render mehtod here:
+    now instead of each square just being a blank tile, it will have it's corresponding number in it.
+    this is because in the Board render() function, we pass each tiles respective value to the renderSquare method which then passes it as a prop to the Square's render method.
+
+
+    the next thing that we want to do is make the squares interactive. In this example we want clicking on the squares to do something.
+    as each square is a button, ofc we need to use onClick.
+    We need to pass a function for the onClick property, and we will do so using JS's 'arrow' (lambda) notation:
+    
+    we started off by just alerting us whenever we click it. But now we want the square to "remember" that is was clicked. 
+    we want this because after it is clicked it should be filled with an "X" mark. To do this we will use "state".
+    
+    In order to give a react component a state, we must set it in its constructor. this.state should ne a private field.
+    Once we have done that, we should changes Square's render method to display the state's value when clicked.
+        replaced this.props.value with this.state.value
+        made the event handler set the state with this.setState({value: 'X'})
+
+    */ 
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" 
+                onClick={
+            ()=>{this.setState({value: 'X'})}}
+        >
+          {this.state.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+    //what does it do? it renders 9 squares.
+    //also creates the method renderSquare which returns a Square object
     renderSquare(i) {
-      return <Square />;
+      // now we have made it so that we are passing a 'prop' called value to the Square.
+      return <Square value ={i}/>;
     }
   
     render() {
@@ -72,6 +103,7 @@ class Square extends React.Component {
   }
   
   class Game extends React.Component {
+      // renders a board with placeholder values.
     render() {
       return (
         <div className="game">
